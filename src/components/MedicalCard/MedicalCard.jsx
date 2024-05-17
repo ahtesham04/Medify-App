34ba6dabfede4ac42deae9e2f22dd07e9a89e4d5
Ahtesham Ansari <ahtesham6867@gmail.com>
2024-05-18 02:18:20 +0530

    functionality added in booking interface

--- src/components/MedicalCard/MedicalCard.jsx
+++ src/components/MedicalCard/MedicalCard.jsx
@@ -1,11 +1,52 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './medicalCard.css';
 import medical from '../../assets/div.u-pos-has.png';
 import like from '../../assets/Vector (1).png'
 import SlotCarausal from '../SlotCarausal/SlotCarausal';
-
+const getLocalStorageData = () =>{
+  let data = JSON.parse(localStorage.getItem("myBooking"))
+  return data
+}
 const MedicalCard = ({data}) => {
 const[showBooking, setShowBooking] = useState(false);
+const[bookingTime, setBookingTime] = useState('')
+const[bookingDate, setBookingDate] = useState('')
+const[bookingData, setBookingData] = useState(getLocalStorageData())
+const dateConversion = (n) =>{
+  let date = new Date();
+  let dateArr = date.toString().split(" ")
+  let day = date.getDate()+n;
+  let formattedDate = `${dateArr[0]}, ${day} ${dateArr[1]}}`
+  return formattedDate;
+}
+const slotDate = (date) =>{
+  if(date === 'Today'){
+    let newDate = dateConversion(0);
+    setBookingDate(newDate)
+  }else if(date === 'Tommorrow'){
+    let newDate = dateConversion(1)
+    setBookingDate(newDate)
+  }else{
+    setBookingDate(date)
+  }
+}
+
+const bookSlotHandler = () =>{
+  let objData = {
+    hospital:data['Hospital Name'],
+    city:data.City,
+    state:data.State,
+    time:bookingTime,
+    date:bookingDate
+  }
+  setBookingData([...bookingData,objData])
+}
+const bookingTimeHandler = (t) =>{
+  setBookingTime(t)
+}
+useEffect(() =>{
+  localStorage.setItem("myBooking",JSON.stringify(bookingData))
+},[bookingData])
   return (
     <div className='card medicalCard' >
         <div className='row' style={{gap:"18px"}} onClick={() => setShowBooking(!showBooking)}>
@@ -23,12 +64,12 @@ const[showBooking, setShowBooking] = useState(false);
         <div className='col-4 d-flex align-items-end'>
           <div>
             <p className='available'>Available Today</p>
-            <button className='bookBtn'>Book Free Center Visit</button>
+            <button className='bookBtn' onClick={bookSlotHandler}>Book Free Center Visit</button>
             </div>
         </div>
         </div>
         {showBooking ? (<div className='slotBooking'>
-        <SlotCarausal />
+        <SlotCarausal bookingDate={slotDate}/>
         <div className='row g-4 mt-2'>
             <div className='col-2'>Morning</div>
             <div className='col-2'><div className='time'>10:00AM </div></div>
